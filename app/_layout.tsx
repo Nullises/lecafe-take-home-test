f3f6f7cf897f57ef308f9d70cf61b40ba08e12db
Ulises Vargas <ext_uvargasds@falabella.cl>
2025-04-22 16:03:05 -0400

    feature/nativewind-and-assets

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,14 @@
-import { View, Text } from "react-native"
+import { View, Text, SafeAreaView } from "react-native"
 import React from "react"
 
 const RootLayout = () => {
   return (
-    <View>
-      <Text>RootLayout</Text>
-    </View>
+    <SafeAreaView>
+      <View>
+        <Text>RootLayout</Text>
+      </View>
+    </SafeAreaView>
+
   )
 }
 
