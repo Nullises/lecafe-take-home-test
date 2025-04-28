0e13c46550aa40c6d58a9eddbbc2b2e0fce5a47a
Ulises Vargas <ext_uvargasds@falabella.cl>
2025-04-28 16:43:30 -0400

    fix/CustomLikeButton

--- components/CustomLikeButton.tsx
+++ components/CustomLikeButton.tsx
@@ -4,24 +4,27 @@ import LikeWhite from "@/assets/icons/LikeWhite"
 import Like from "@/assets/icons/Like"
 
 interface CustomButtonProps extends PressableProps {
+
 }
 
 const CustomLikeButton = ({
     onPress,
 }: CustomButtonProps) => {
 
-    const [btnPressed, setBtnPressed] = useState(false)
+    const [btnLikePressed, setBtnLikePressed] = useState(false)
 
     return (
         <Pressable
             onPress={onPress}
+            onPressIn={() => setBtnLikePressed(true)}
+            onPressOut={() => setBtnLikePressed(false)}
             style={({ pressed }) => ({
                 opacity: pressed ? 0.7 : 1
             })
             }
             className={`p-4 rounded-full flex justify-center items-center w-[58px] h-[58px] active:bg-buttonstrongpink bg-buttonwhite`}
         >
-            {btnPressed ? (<LikeWhite />) : (<Like />)}
+            {btnLikePressed ? (<LikeWhite />) : (<Like />)}
         </Pressable>
     )
 }
